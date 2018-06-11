ef480effcae2a7f05b83850a096fc6f83bb36fad
bogdanmandic <bogdan.mandic@riitech.com>
2018-06-11 16:58:23 +0200

    Pretraga po tags radi, Stage 2 done

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,45 @@ var upload = multer({ dest: 'uploads/' })
 var Image = require('../models/Image');
 
 
+var slike = [
+  slika1 = ['ceca', 'deca', 'nista'],
+  slika2 = ['sise', 'velike', 'nisu'],
+  slika3 = ['actress']
+]
+
+var mozeDaProdje = []
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.post('/imagesWithTags', async (req, res) => {
+  let { tags } = req.body;
+  tags = Array.isArray(tags) ? tags : tags.split();
+  Image.find({}, (err, allImages) => {
+    if (err) res.json(err);
+    allImages.forEach(s => {
+      var poklopljeniTagovi = 0;
+      console.log(s)
+      s.tags.forEach(ms => {
+        tags.forEach(tag => {
+          if (tag == ms) {
+            poklopljeniTagovi += 1
+          }
+        })
+      })
+      if (poklopljeniTagovi != 0) {
+        mozeDaProdje.push([poklopljeniTagovi + ',' + (s)])
+      }
+    })
+    var newArray = mozeDaProdje.sort()
+    var reversed = newArray.reverse()
+    res.json(reversed)
+    mozeDaProdje = []
+    // console.log('=-========', mozeDaProdje)
+  });
 });
 
 router.post('/profile', upload.single('banner'), (req, res, next) => {
